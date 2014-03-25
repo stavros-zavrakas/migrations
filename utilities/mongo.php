645e52780670e5f8f401fa3ba31668924d2a08f5
Stavros Zavrakas <stavros.zavrakas@styloola.com>
2014-03-25 16:03:59 +0100

    return back mongo connectin with db name

--- utilities/mongo.php
+++ utilities/mongo.php
@@ -2,17 +2,20 @@
 
 	class stlMongo {
 		protected $connection;
+		private $mongoDb;
 		private $mongoStr;
 
 		public function __construct($mongoArray) {
 	  	$mongoStr = $mongoArray['host'] . ':' . $mongoArray['port'] . '/' . $mongoArray['database'];
 	  	$this->mongoStr = $mongoStr;
+	  	$this->mongoDb = $mongoArray['database'];
 		}
 
 		public function getMongoConnection() {
 	    try {
 	      $this->connection = new MongoClient($this->mongoStr);
-	      return $this->connection;
+	      $dbName = $this->mongoDb;
+	      return $this->connection->$dbName;
 	    } catch (Exception $e) {
 	      echo 'Mongo Connection failed: ',  $e->getMessage(), "\n";
 	      return false;
